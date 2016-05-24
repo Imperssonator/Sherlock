714432d1f470dd3e742a1a257c4fbb884326ddd0
Perry W Ellis <perrywmellis@gmail.com>
2016-05-24 15:10:05 -0400

    added function to return dominat color in an image

--- functions/bin_color.py
+++ functions/bin_color.py
@@ -2,6 +2,8 @@ import numpy as np
 from matplotlib import cm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import cluster_data as cld
+from skimage.measure import regionprops
 # from cluster_data import plot_colors
 
 # colorIm = cm.PuRd(np.arange(256))
@@ -24,6 +26,20 @@ def bin_colors(im, bin_per_axis=4):
     labeled_im = labeled_im.reshape((w, h))
     plt.figure()
     plt.imshow(labeled_im, cmap='viridis')
+
+
+def find_dom_color(im, pltcolor=False):
+    recon_im, label_im = cld.cluster_colorspace_km(im, n_clusters=2,
+                                                   train_size=2000)
+    if pltcolor is True:
+        cld.plot_colors(recon_im, title_text='k=2 colors from find_dom_color')
+        plt.figure()
+        plt.imshow(label_im, cmap='viridis')
+        plt.suptitle('label im from find_dom_color')
+    regions = regionprops(label_im)
+    areas, labels = np.split(np.array([[i.area, i.label] for i in regions]),
+                             2, axis=1)
+    return label_im == labels[np.argmax(areas)]
 
 
 if __name__ == '__main__':
