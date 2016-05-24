4d9f95fddd8f1b0b39454272d7c73371792f3b45
Perry W Ellis <perrywmellis@gmail.com>
2016-05-24 15:10:47 -0400

    changed to start testing background dilation on colorspace

--- functions/diagnose.py
+++ functions/diagnose.py
@@ -7,8 +7,10 @@ from mpl_toolkits.mplot3d import Axes3D
 import scipy.ndimage as ndi
 import single_series_scrape as sss
 from matplotlib import cm
-from bin_color import bin_colors
+from bin_color import find_dom_color
 import cluster_data as cld
+import skimage.morphology as mph
+
 
 testim_file = '../examples/isotherm3.jpg'
 xaxes = [[97, 461], [583, 945]]
@@ -37,8 +39,22 @@ ax1.imshow(ROIs[0])
 ax2.imshow(ROIs[1])
 ax1.set_title('First ROI, OG image')
 ax2.set_title('Second ROI, OG image')
+st = np.ones((3, 3))  # 2D connectivity structure element
 
 for ii, i in enumerate(ROIs):
-    cld.plot_colors(i, title_text='LAB space, ROI {0}'.format(ii),
-                    tform=color.rgb2lab)
+    if ii == 1:
+        cld.plot_colors(i, title_text='RGB space, ROI {0}'.format(ii),
+                        tform=None)
+        cld.plot_colors(i, title_text='LAB space, ROI {0}'.format(ii),
+                        tform=color.rgb2lab)
+        cld.plot_colors(i, title_text='XYZ space, ROI {0}'.format(ii),
+                        tform=color.rgb2xyz)
+        bknd_mask = find_dom_color(i, pltcolor=False)
+        plt.figure()
+        plt.imshow(bknd_mask, cmap='viridis')
+        plt.suptitle('no erosion')
+        bknd_mask = mph.dilation(bknd_mask, selem=st)
+        plt.figure()
+        plt.imshow(bknd_mask, cmap='viridis')
+        plt.suptitle('one dilation')
 plt.show()
