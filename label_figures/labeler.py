b30ed9ef0932d4bd343f797984d19e4bee5287d9
saganaut <robert.s.pienta@gmail.com>
2016-05-29 17:40:51 -0400

    added a few more features, x and y axis mainly

--- label_figures/labeler.py
+++ label_figures/labeler.py
@@ -20,11 +20,19 @@ def present_images(img_dir, dataset, conn, cur):
     img = Image.open(img_path)
     img.show()
     num_plots = return_default_if_empty(raw_input('Approx num graphics (def 2)?'), 2)
+    plots = return_default_if_empty(raw_input('Are there plots? (default:yes)'), 1)
+    if plots:
+      x_ax = return_default_if_empty(raw_input('Plot has x-axis? (default:yes)'), 1)
+      y_ax = return_default_if_empty(raw_input('Plat has y-axis? (default:yes)'), 1)
+    else:
+      x_ax = 0 
+      y_ax = 0
     has_img = return_default_if_empty(raw_input('Contains any images (default:yes=1)?'), 1) 
     has_diag = return_default_if_empty(raw_input('Contains any diagrams (default:yes=1)?'), 1) 
     has_color = return_default_if_empty(raw_input('Has color in the plot (default:yes=1)?'), 1)
+
     print img_file, dataset, has_img, num_plots, has_color, has_diag
-    cur.execute("INSERT INTO img_labels VALUES('{0}', '{1}', {2}, {3}, {4}, {5})".format(img_file, dataset, has_img, num_plots, has_color, has_diag))
+    cur.execute("INSERT INTO img_labels VALUES('{0}', '{1}', {2}, {3}, {4}, {5}, {6}, {7}, {8})".format(img_file, dataset, num_plots, has_img, plots, x_ax, y_ax, has_color, has_diag))
     conn.commit()
 
 def return_default_if_empty(val, default_val):
@@ -38,12 +46,14 @@ def get_db_cursor():
   conn = sql.connect('../data/image_labels.db')
   cur = conn.cursor()
   # Create the db with cols:            
-  cur.execute("""CREATE TABLE IF NOT EXISTS img_labels (file_name text, dataset text, has_image integer, num_graphics integer, 
-                has_color integer, has_diagram integer) """)
+  cur.execute("""CREATE TABLE IF NOT EXISTS img_labels (file_name text, dataset text, 
+              num_graphics integer, has_image integer, has_plot integer, 
+              has_x_axis integer, has_y_axis integer, has_color integer, 
+              has_diagram integer) """)
   return conn, cur
 
 def row_exists(cur, path):
-  cur.execute("SELECT * FROM img_labels WHERE file_path = '" + path + "';")
+  cur.execute("SELECT * FROM img_labels WHERE file_name= '" + path + "';")
   return cur.fetchone() != None 
   
 
