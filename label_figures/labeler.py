b90b2b18934f6e5a01174a28e8d5597272b4ea32
saganaut <robert.s.pienta@gmail.com>
2016-06-01 20:33:57 -0400

    labeler changes

--- label_figures/labeler.py
+++ label_figures/labeler.py
@@ -21,18 +21,12 @@ def present_images(img_dir, dataset, conn, cur):
     img.show()
     num_plots = return_default_if_empty(raw_input('Approx num graphics (def 2)?'), 2)
     plots = return_default_if_empty(raw_input('Are there plots? (default:yes)'), 1)
-    if plots:
-      x_ax = return_default_if_empty(raw_input('Plot has x-axis? (default:yes)'), 1)
-      y_ax = return_default_if_empty(raw_input('Plat has y-axis? (default:yes)'), 1)
-    else:
-      x_ax = 0 
-      y_ax = 0
     has_img = return_default_if_empty(raw_input('Contains any images (default:yes=1)?'), 1) 
     has_diag = return_default_if_empty(raw_input('Contains any diagrams (default:yes=1)?'), 1) 
     has_color = return_default_if_empty(raw_input('Has color in the plot (default:yes=1)?'), 1)
 
     print img_file, dataset, has_img, num_plots, has_color, has_diag
-    cur.execute("INSERT INTO img_labels VALUES('{0}', '{1}', {2}, {3}, {4}, {5}, {6}, {7}, {8})".format(img_file, dataset, num_plots, has_img, plots, x_ax, y_ax, has_color, has_diag))
+    cur.execute("INSERT INTO img_labels VALUES('{0}', '{1}', {2}, {3}, {4}, {5}, {6})".format(img_file, dataset, num_plots, has_img, plots, has_color, has_diag))
     conn.commit()
 
 def return_default_if_empty(val, default_val):
@@ -48,8 +42,7 @@ def get_db_cursor():
   # Create the db with cols:            
   cur.execute("""CREATE TABLE IF NOT EXISTS img_labels (file_name text, dataset text, 
               num_graphics integer, has_image integer, has_plot integer, 
-              has_x_axis integer, has_y_axis integer, has_color integer, 
-              has_diagram integer) """)
+              has_color integer, has_diagram integer) """)
   return conn, cur
 
 def row_exists(cur, path):
